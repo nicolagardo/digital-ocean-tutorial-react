4efeb7ca94fe92f01ec84508829dec4102e7c240
nicolagardo <nicolagardo@gmail.com>
2022-04-29 00:22:03 -0300

    Step 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 
 function App() {
+  const greeting = "greeting";
   return (
     <>
-      <h1>Hello, World</h1>
-      <p>I am writing JSX</p>
+      <div className="container">
+        <h1 id={greeting}>Hello, World</h1>
+        <p>I am writing JSX</p>
+        <ul>
+          <li>
+            <button>
+              <span role="img" aria-label="grinning face" id="grinning face">😀</span>
+            </button>
+            </li>
+          <li>
+            <button>
+                <span role="img" aria-label="party popper" id="party popper">🎉</span>
+            </button>
+          </li>
+          <li>
+            <button>
+                <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
+            </button>
+          </li>
+        </ul>
+      </div>
     </>
+    
     
   ) 
 
