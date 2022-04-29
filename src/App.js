3fa02e22dc1a405e27091b623e3f8c18913d10f8
nicolagardo <nicolagardo@gmail.com>
2022-04-29 00:37:48 -0300

    add function once and passing it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 function App() {
   const greeting = "greeting";
+  const clickeame = () =>{}
   return (
     <>
       <div className="container">
@@ -9,17 +10,17 @@ function App() {
         <p>I am writing JSX</p>
         <ul>
           <li>
-            <button>
+            <button onClick={event => alert(event.target.id)}>
               <span role="img" aria-label="grinning face" id="grinning face">😀</span>
             </button>
             </li>
           <li>
-            <button>
+            <button onClick={event => alert(event.target.id)}>
                 <span role="img" aria-label="party popper" id="party popper">🎉</span>
             </button>
           </li>
           <li>
-            <button>
+            <button onMouseOverCapture={ev => console.log(ev.target.id)}>
                 <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
             </button>
           </li>
